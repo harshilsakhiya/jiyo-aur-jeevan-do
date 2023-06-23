ab96f82144a48f950debdd8e8a1dc04a2a0f4e3b
Harshil Sakhiya <harshilsakhiya9886@gmail.com>
2023-06-24 00:45:58 +0530

    table heder change

--- src/views/tables/TableStickyHeader.js
+++ src/views/tables/TableStickyHeader.js
@@ -13,29 +13,16 @@ import TablePagination from '@mui/material/TablePagination'
 import { Button } from '@mui/material'
 
 const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { id: 'name', label: 'User\u00a0Id', minWidth: 170 },
+  { id: 'code', label: 'Email\u00a0Id', minWidth: 100 },
   {
     id: 'population',
-    label: 'Population',
+    label: 'Password',
     minWidth: 170,
     align: 'right',
     format: value => value.toLocaleString('en-US')
   },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: value => value.toLocaleString('en-US')
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: value => value.toFixed(2)
-  },
+  
 
 ]
 function createData(name, code, population, size) {
